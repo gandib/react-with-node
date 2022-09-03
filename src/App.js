ab08d96060ccfec05066c6f1a159f066f6031db2
Gandib Roy <gandibroy11@gmail.com>
2022-09-03 21:02:04 +0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+
+function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+
+    fetch('http://localhost:5000/user', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+        console.log(data);
+      })
+
+  }
+
+  return (
+    <div className="App">
+      <h1>My own data {users.length}</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' placeholder='Name' required /><br />
+        <input type="email" name='email' placeholder='Email' required /><br />
+        <input type="submit" value="Add User" />
+      </form>
+      <ul>
+        {
+          users.map(user => <li key={user.id}>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
